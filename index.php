7ceef571620c5c2226669288fefb716e8a63ca32
skr1pmen <KiberKot.v.roymenge@gmail.com>
2023-07-06 20:35:37 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php
+
+
+for ($i = 1; $i <= 100; $i++){
+    if ($i % 2 == 0){
+        echo $i . "";
+    }
+}
+echo "<hr>";
+$i = 1;
+while ($i <= 100){
+    if ($i % 2 == 0){
+        echo $i . "";
+    }
+    $i++;
+}
+echo "<hr>"
+//$a = 1;
+//$b = 10;
+//
+//if ($a % $b == 0){
+//    echo "Число делится!";
+//}
+//else{
+//    echo "У числа есть остаток!";
+//}
+
+//$year = 18;
+//if ($year % 4 and $year % 400){
+//    echo "Год високосный!";
+//}
+//else{
+//    echo "Год не високосный!";
+//}
+
+//$month = 1;
+//switch ($month){
+//    case 12:
+//    case 1:
+//    case 2: echo 'Зима'; break;
+//    case 3:
+//    case 4:
+//    case 5: echo 'Весна'; break;
+//    case 6:
+//    case 7:
+//    case 8: echo 'Лето'; break;
+//    case 9:
+//    case 10:
+//    case 11: echo 'Осень'; break;
+//    default: echo 'Не верный диапозон!';
+//}
+//$operation = '2';
+//switch ($operation){
+//    case '1': echo 'Зима'; break;
+//    case '2': echo 'Лето'; break;
+//    case '3': echo 'Осень'; break;
+//    case '4': echo 'Весна'; break;
+//    default: echo 'Введите число от 1 до 4';
+//}
+//$x = 0;
+//$y = 0;
+//if ($x>0 and $y>0){
+//    echo 'Первая четверть!';
+//}
+//elseif ($x>0 and $y<0)
+//    echo 'Вторая четверть!';
+//elseif ($x<0 and $y<0)
+//    echo 'Третья четверть!';
+//elseif ($x<0 and $y>0){
+//    echo 'Четвертая четверть!';
+//}
+//else{
+//    echo 'Мы в центре!';
+//}
+//$a = 7;
+//$b = 3;
+//$operation = '-';
+//switch ($operation){
+//    case '+': echo $a + $b; break;
+//    case '-': echo $a - $b; break;
+//    case '*': echo $a * $b; break;
+//    case '/': echo $a / $b; break;
+//    default: echo 'Все посчитано!';
+//}
+//
+//$a = $_GET['a'];
+//$b = $_GET['b'];
+//$operation = $_GET['operation'];
+//
+//if ($operation == "+"){
+//    $result = $a + $b;
+//}
+//elseif ($operation == "-"){
+//    $result = $a - $b;
+//}
+//elseif ($operation == "*"){
+//    $result = $a * $b;
+//}
+//elseif ($operation == "/"){
+//    if ($b != 0)
+//        $result = $a / $b;
+//    }   else{
+//        $result = "Ошибка деление на ноль!";
+//    }
+//?>
+<!--<form method="get">-->
+<!--    <input type="text" name="a" placeholder="Первое число">-->
+<!--    <select name="operation" id="op">-->
+<!--        <option value="+">Сложение</option>-->
+<!--        <option value="-">Вычетание</option>-->
+<!--        <option value="*">Умножение</option>-->
+<!--        <option value="/">Деление</option>-->
+<!--    </select>-->
+<!--    <input type="text" name="b" placeholder="Второе число">-->
+<!--    <input type="submit" value="=">-->
+<!--</form>-->
